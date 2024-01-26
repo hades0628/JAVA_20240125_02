5fcebf13b5fe878c0e343b6b6568457af112c89d
hades0628 <9906kim@naver.com>
2024-01-26 09:42:43 +0900

    message

---
+++ src/ex01/StringArray01.java
@@ -0,0 +1,17 @@
+package ex01;
+
+public class StringArray01 {
+
+	public static void main(String[] args) {
+		String[] myArr= new String[3];
+		
+		myArr[0] = "kor";
+		myArr[1] = "eng";
+		
+		for(int i=0; i<3; i++) {
+			System.out.print(myArr[i] + " ");
+		}
+
+	}
+
+}
